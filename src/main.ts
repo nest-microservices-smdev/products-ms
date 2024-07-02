971a58945314a05c2595d89d1afe0f8aa80d51b4
Samir Mahmud <samir93bj@gmail.com>
2024-07-01 23:11:06 -0300

    chore: Add environment configuration for port
    - Create a new file `envs.ts` in the `config` directory to handle environment variables.
    - Define the `PORT` variable with a default value of 3000 using `joi` validation.
    - Import and export the `envs` object in the `index.ts` file.
    - Update the `main.ts` file to use the `envs.port` variable when listening for incoming requests.
    
    Closes #123

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(envs.port);
 }
 bootstrap();
