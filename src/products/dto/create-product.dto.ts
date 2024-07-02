58ce569c6fe4e1902363ae47039c9c6c054f0dc8
Samir Mahmud <samir93bj@gmail.com>
2024-07-01 22:56:06 -0300

    feat: Add validation for product creation
    - Add validation for the `CreateProductDto` class to ensure that the `name` property is a string and the `price` property is a number with a maximum of 4 decimal places and a minimum value of 0.
    - Apply the `ValidationPipe` globally in the `main.ts` file to enforce the validation rules for incoming requests.

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,14 @@
-export class CreateProductDto {}
+import { Type } from 'class-transformer';
+import { IsString, IsNumber, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  public name: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 4,
+  })
+  @Min(0)
+  @Type(() => Number)
+  public price: number;
+}
